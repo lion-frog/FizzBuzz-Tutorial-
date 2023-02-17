4975f56b46a1d315363e5704cd0fc200fc553115
Your Name <you@example.com>
2023-02-16 19:55:57 -0500

    fizzbuzz code update

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <=30; i++) {
 if (i % 3 === 0 && i % 5 === 0) {
     console.log ('FizzBuzz');
-} else if i % 5 === 0){
+} else if (i % 5 === 0) {
     console.log ('Buzz');
 } else if (i % 3 === 0) {
 console.log('Fizz');
